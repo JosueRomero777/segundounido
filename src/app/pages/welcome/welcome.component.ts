c8d026d317ee8dd55b5f88446cdb1be69c129b76
Mateo Lituma <milituma@sudamericano.edu.ec>
2024-11-21 11:53:24 -0500

    mateo-l

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { DiazcheckComponent } from '../../diazcheck/diazcheck.component';
 import { SelectComponent } from '../../select/select.component';
+import { MateoLComponent } from '../../mateo-l/mateo-l.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
-  imports: [DiazcheckComponent, SelectComponent]
+  imports: [DiazcheckComponent, SelectComponent, MateoLComponent]
 })
 export class WelcomeComponent implements OnInit {
 
