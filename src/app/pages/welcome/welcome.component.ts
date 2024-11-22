531a9b08e42c0b967117dff283f1a4527d1e8fce
Wilson-1 <wpcriollo@sudamericano.edu.ec>
2024-11-21 20:05:20 -0500

    Wilson

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ZambranoComponent } from '../../zambrano/zambrano.component';
 import { NzAlertComponent } from '../../PaulGuti/nz-alert.component';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 @Component({
-  selector: 'app-welcome',
   standalone: true,
+  selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
-  imports: [ ZambranoComponent, NzAlertComponent]
+  imports: [ZambranoComponent, NzAlertComponent,NzAvatarModule, NzDividerModule, NzToolTipModule]
 })
 export class WelcomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() { }
-
+  ngOnInit(): void {}
 }
